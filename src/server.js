fa898d9b3426c3767fae429aa932b8f2247c8261
CarlosXavier <cxcarvaj@espol.edu.ec>
2020-10-04 01:28:37 -0500

    GET products

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import express from 'express';
+const axios = require('axios');
 
 const app = express();
 const prismaClient = new PrismaClient();
@@ -9,6 +10,17 @@ app.get('/users', async (req, res) => {
   res.json(users);
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);
+app.get('/products/:country', async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://world.openfoodfacts.org/country/${req.params.country}.json`
+    );
+    console.log(response.data);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error.response.body);
+    res.status(400).json(error.response.body);
+  }
+});
+
+app.listen(3000, () => console.log(`Example app listening on port ${3000}!`));
